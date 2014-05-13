ab23040688f5aa0a2d3323c53db9e0d3417b0627
domicode <domi@domidesign.ch>
2014-05-13 18:18:48 -0400

    added editing contacts funciton

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,43 @@
 class ContactsController < ApplicationController
 
+  def new
+    @user = User.find(params[:user_id])
+    @contact = Contact.new(:user_id=>@user.id)
+  end
 
   def create
     @user = User.find(params[:user_id])
-    @contact = @user.contacts.create!(comment_params)
+    @contact = @user.contacts.create!(contact_params)
     redirect_to @user, notice: "contact was created"
   end
+
+  
+  def edit
+    @user = User.find(params[:user_id])
+    @contact = @user.contacts.first
+  end
+
+  
+  def update
+    @user = User.find(params[:user_id])
+    respond_to do |format|
+      if @user.contacts.first.update(contact_params)
+        format.html { redirect_to @user, notice: 'Contact was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
 
 
 
   private
 
-  def comment_params
+  def contact_params
     params.require(:contact).permit(:first_name, :last_name, :street, :postal_code, :city, :country)
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,28 +3,21 @@ class UsersController < ApplicationController
   before_filter :require_login, :except => [:not_authenticated]
   skip_before_filter :require_login, only: [:index, :new, :create]
 
-  # GET /users
-  # GET /users.json
   def index
-    @users = User.all
+    redirect_to user_path(current_user.id)
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -39,8 +32,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -53,8 +44,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
@@ -64,12 +53,10 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:first_name, :last_name, :street, :postal_code, :city, :country, :email, :password, :password_confirmation)
     end
@@ -77,4 +64,5 @@ class UsersController < ApplicationController
     def not_authenticated
       redirect_to root_path, :alert => "Please login first"
     end
+    
 end
