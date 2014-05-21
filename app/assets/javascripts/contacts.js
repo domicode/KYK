68949a90b89623ba1a2044f297801abc237edb48
Brian Simon <tarsipix@gmail.com>
2014-05-21 17:25:27 -0400

    uncommented javascript for contacts

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -2,18 +2,18 @@ $(document).ready(function(){
 
   $('#contact_tag_list').tagsInput();
 
-  // // Function to show contacts in full size and small size with a css trigger
-  // $(".contact").click(function(event){
-  //   if ($(this).height() > 100 ) {
-  //     // $(this).css('height', '100px');
-  //     $(this).addClass("contact")
-  //     $(this).removeClass("contactfullsize")
-  //   } else {
-  //     // $(this).css('height', 'auto');
-  //     $(this).addClass("contactfullsize")
-  //     $(this).removeClass("contact")
-  //   }
-  // });
+  // Function to show contacts in full size and small size with a css trigger
+  $(".contact").click(function(event){
+    if ($(this).height() > 100 ) {
+      // $(this).css('height', '100px');
+      $(this).addClass("contact")
+      $(this).removeClass("contactfullsize")
+    } else {
+      // $(this).css('height', 'auto');
+      $(this).addClass("contactfullsize")
+      $(this).removeClass("contact")
+    }
+  });
 
 
 
